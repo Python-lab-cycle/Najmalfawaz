f2ee63eac918a67a74f702e21f4e7b17ce0aadbb
Najmalfawaz <76723512+Najmalfawaz@users.noreply.github.com>
2021-02-25 11:20:51 +0530

    Add files via upload

---
+++ filecopy.py
@@ -0,0 +1,11 @@
+file1=input("enter the source file to be copied")
+file2=input("enter the destination file name")
+f1= open(file1,"r")
+f2=open(file2,"w")
+for line in f1.readline():
+	f2.write(line)
+f1.close()
+
+f1=open(file1,"r")
+line=f1.read()
+print("line",line)
